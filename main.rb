a04dccc03458b125f7c2ce206632e688f398c28f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2015-05-17 22:09:05 +0800

    draw whole board

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ class ChineseCheckersWindow < Gosu::Window
   def initialize
     super(640, 480, false)
     self.caption = 'Hello'
-    @board = Board.new(200, 0, 30, 20)
+    @board = Board.new(300, 20, 30, 20)
     @board.start_game(6)
     @message = Gosu::Image.from_text(self, 'Hello, World!', Gosu.default_font_name, 30)
   end
