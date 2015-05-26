a07029b83ec5897c9373c88e92d2a82015213cd9
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2015-05-26 18:51:53 +0800

    test c lib

---
+++ lib/core.c
@@ -0,0 +1,6 @@
+#include "math.h"
+#include <stdlib.h>
+#include "memory.h"
+int sum(int num1, int num2, int num3){ //test
+  return num1 + num2 + num3;
+}
---
+++ lib/core.rb
@@ -0,0 +1,9 @@
+require 'dl'
+require 'dl/import'
+module Core
+  extend DL::Importer
+  dlload File.expand_path('../core.dll', __FILE__)
+  extern 'int sum(int, int, int)'
+end
+#Test
+p Core.sum(1,5,100)
